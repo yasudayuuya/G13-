c7f40936fb58f036114fae9dd1ea0f8845fde1c5
yasudayuuya <s2322092@stu.musashino-u.ac.jp>
2024-01-14 20:13:34 +0900

    気象庁のWebページを取得

--- temperature.py
+++ temperature.py
@@ -2,4 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
+url = "https://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php?prec_no=45&block_no=1236&year=2024&month=1&day=&view="
+d_list = []
+time.sleep(1)
 
+res = requests.get(url)
+html = res.text
+soup = BeautifulSoup(res.text, 'html.parser')
