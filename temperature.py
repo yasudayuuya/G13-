7ef2904a890e51a98be3f46af7b71960c848dd63
yasudayuuya <s2322092@stu.musashino-u.ac.jp>
2024-01-15 18:04:04 +0900

    気象庁の時刻と気温のデータをスクレイピング

--- temperature.py
+++ temperature.py
@@ -2,10 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-url = "https://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php?prec_no=45&block_no=1236&year=2024&month=1&day=&view="
-d_list = []
-time.sleep(1)
+url = "https://www.data.jma.go.jp/obd/stats/etrn/view/hourly_a1.php?prec_no=45&block_no=1236&year=2024&month=01&day=1&view=p1"
+temperature_data = []
+time.sleep(0.1)
 
 res = requests.get(url)
 html = res.text
 soup = BeautifulSoup(res.text, 'html.parser')
+time.sleep(0.1)
+
+for tr in soup.find_all('tr', class_='mtx'):
+    td_list = tr.find_all('td')
+    if len(td_list) >= 3:
+        time = td_list[0].text.strip()
+        temperature = td_list[2].text.strip()
+        temperature_data.append((time, temperature))
+
+for time, temperature  in temperature_data:
+    print(f"Time: {time}, Temperature: {temperature}")
