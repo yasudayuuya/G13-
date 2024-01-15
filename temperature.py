7338cee80fd8f5c70974d15c2451d94a26bf2102
yasudayuuya <s2322092@stu.musashino-u.ac.jp>
2024-01-16 02:11:08 +0900

    オープンデータをDBに格納

--- temperature.py
+++ temperature.py
@@ -14,9 +14,35 @@ time.sleep(0.1)
 for tr in soup.find_all('tr', class_='mtx'):
     td_list = tr.find_all('td')
     if len(td_list) >= 3:
-        time = td_list[0].text.strip()
-        temperature = td_list[2].text.strip()
-        temperature_data.append((time, temperature))
+        time_values = td_list[0].text.strip()
+        temperature_values = td_list[2].text.strip()
+        temperature_data.append({'time': time_values, 'temperature': temperature_values})
 
-for time, temperature  in temperature_data:
-    print(f"Time: {time}, Temperature: {temperature}")
+for item  in temperature_data:
+    print(f"Time: {item['time']}, Temperature: {item['temperature']}")
+
+import sqlite3
+
+con = sqlite3.connect('google_database.db')
+cur = con.cursor()
+
+sql_createtable_DSp = 'CREATE TABLE IF NOT EXISTS sql_DS (time TEXT, temperature TEXT)'
+cur.execute(sql_createtable_DSp)
+
+for item in temperature_data:
+    cur.execute('INSERT INTO sql_DS (time, temperature) VALUES (?, ?)', (item['time'], item['temperature']))
+
+con.commit()
+
+con.close()
+
+con = sqlite3.connect('google_database.db')
+cur = con.cursor()
+
+cur.execute('SELECT * FROM sql_DS')
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+con.close() 
