0ec72fb207de7464f7c704b42ece3e012f7d2a92
yasudayuuya <s2322092@stu.musashino-u.ac.jp>
2024-01-16 10:15:23 +0900

    オープンデータをデータベースに格納する

---
+++ out_temperature.py
@@ -0,0 +1,29 @@
+import csv
+import pandas as pd
+import sqlite3
+
+con = sqlite3.connect('local_database.db')
+cur = con.cursor()
+
+sql_createtable_local = 'CREATE TABLE IF NOT EXISTS local_data (column1 TEXT, column2 TEXT, column3 TEXT)'
+cur.execute(sql_createtable_local)
+
+with open('/Users/yasudayuuya/class/DSprogramming/DSfinal/local.csv', 'r', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+    for row in csv_reader:
+        cur.execute('INSERT INTO local_data (column1, column2, column3) VALUES (?, ?, ?)', (row[0], row[1], row[2]))
+
+con.commit()
+
+con.close()
+
+con = sqlite3.connect('local_database.db')
+cur = con.cursor()
+
+cur.execute('SELECT * FROM local_data')
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+con.close()
